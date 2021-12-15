0922a7f662cec14ffed78946a3dd0f44b3e4fc0b
Toya <toyack1128@gmail.com>
2021-12-15 21:36:16 +0900

    first

---
+++ layouts/default.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+    <v-footer
+     color="#c5eff7"
+    padless
+    >
+        <v-col
+        color="primary lighten-1"
+        class=" text-center"
+        cols="12">
+          {{ new Date().getFullYear() }} — <strong>Bluwm</strong>
+        </v-col>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'DefaultLayout',
+  data() {
+    return {
+     
+    }
+  },
+}
+</script>
