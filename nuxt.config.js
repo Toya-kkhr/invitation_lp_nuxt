172d21477eecf339e72c15c60ad47828144d38ca
Toya <toyack1128@gmail.com>
2022-02-18 22:50:46 +0900

    20220218

--- nuxt.config.js
+++ nuxt.config.js
@@ -20,6 +20,8 @@ export default {
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: ['@/assets/css/style.css'],
 
+  ssr: true,
+
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
     { src:'@/plugins/vee-validate', ssr: false}
