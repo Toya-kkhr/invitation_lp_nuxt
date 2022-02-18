604d28f7bd2873550bec89bdf1df8093f0e2ab6c
Toya <toyack1128@gmail.com>
2022-02-18 23:22:34 +0900

    20220218

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,6 +21,7 @@ export default {
   css: ['@/assets/css/style.css'],
 
   ssr: true,
+  target: 'server',
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
