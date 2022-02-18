90917a6ca923769f034c053abcf02b42942eb91f
Toya <toyack1128@gmail.com>
2022-02-19 00:00:26 +0900

    20220218

--- components/home-form.vue
+++ components/home-form.vue
@@ -117,6 +117,29 @@ class="text-center"
     </validation-observer>
 
         </div>
+
+        <form name="contact" method="POST" data-netlify="true">
+  <p>
+    <label>Your Name: <input type="text" name="name" /></label>   
+  </p>
+  <p>
+    <label>Your Email: <input type="email" name="email" /></label>
+      <label>Your Email: <input type="email" name="message" /></label>
+        <label>Your Email: <input type="email" name="botfield" /></label>
+  </p>
+  <p>
+    <label>Your Role: <select name="role[]" multiple>
+      <option value="leader">Leader</option>
+      <option value="follower">Follower</option>
+    </select></label>
+  </p>
+  <p>
+    <label>Message: <textarea name="message"></textarea></label>
+  </p>
+  <p>
+    <button type="submit">Send</button>
+  </p>
+</form>
         </v-sheet>
 
 
