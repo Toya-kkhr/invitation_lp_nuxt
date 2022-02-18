e58e33d8ff53f51a0c4c2cfc2aa20105dc5f9e20
Toya <toyack1128@gmail.com>
2022-02-18 23:54:35 +0900

    20220218

--- components/home-form.vue
+++ components/home-form.vue
@@ -117,19 +117,6 @@ class="text-center"
     </validation-observer>
 
         </div>
-
-        <form name="contact" netlify>
-            <input type="hidden" name="form-name" :value="contact"/>
-  <p>
-    <label>Name <input type="text" name="name" /></label>
-  </p>
-  <p>
-    <label>Email <input type="email" name="message" /></label>
-  </p>
-  <p>
-    <button type="submit">Send</button>
-  </p>
-</form>
         </v-sheet>
 
 
